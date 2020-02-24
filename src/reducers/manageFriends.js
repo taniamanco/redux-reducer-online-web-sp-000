4981d7cb2eb991842235624349ab04a95876e09e
Tania Manco <belalov.tania16@gmail.com>
2020-02-24 19:03:44 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,3 +1,9 @@
 export function manageFriends(state = {friends: []}, action){
-  if(action.type === "")
+  if(action.type === "ADD_FRIEND"){
+    return({...state, friends: [...state.friends], action.friends})
+  } else if(){
+    
+  } else {
+    
+  }
 }
