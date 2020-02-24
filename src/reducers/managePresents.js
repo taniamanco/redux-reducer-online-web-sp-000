3fe0af34f220291af90cdfae961002fa266d82b7
Tania Manco <belalov.tania16@gmail.com>
2020-02-24 18:58:33 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,6 +1,7 @@
 export function managePresents(state={numberOfPresents: 0}, action){
   if(action.type === "INCREASE") {
-    
-    
+    return Object.assign({}, state, {
+      numberOfPresents: state.numberOfPresents +1
+    })
   }
 }
